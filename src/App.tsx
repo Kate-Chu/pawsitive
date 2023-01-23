1068ce3cff3a0e23d5e98f1f6595f3e66f257e77
Kate <chu.shihchien@gmail.com>
2023-01-23 20:29:56 +0800

    chore: install prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { styled } from "@linaria/react";
-import logo from "./logo.svg";
+import React from 'react';
+import { styled } from '@linaria/react';
+import logo from './logo.svg';
 
 const App: React.FC = () => {
   return (
