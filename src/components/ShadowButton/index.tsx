1e0088d6e61fbb38fd59c250a6eb1bdf54f3b234
Kate <chu.shihchien@gmail.com>
2023-01-24 20:49:24 +0800

    style: add shadowed-button style

--- src/components/ShadowButton/index.tsx
+++ src/components/ShadowButton/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { styled } from '@linaria/react';
+import { COLOR } from 'src/styles/base';
 
 type ShadowButtonProps = {
   children?: React.ReactNode;
@@ -7,7 +9,21 @@ type ShadowButtonProps = {
 const ShadowButton: React.FC<ShadowButtonProps> = (props) => {
   const { children } = props;
 
-  return <button className="shadow-button">{children}</button>;
+  return (
+    <StyledShadowButton className="shadow-button">
+      {children}
+    </StyledShadowButton>
+  );
 };
+
+const StyledShadowButton = styled.button`
+  background-color: ${COLOR.orange}};
+  padding: 0.5rem 1rem;
+  border-radius: 0.2rem;
+  color: ${COLOR.white};
+  font-size: 1.2rem;
+  font-weight: 600;
+  box-shadow: 6.8px 6.8px 0 ${COLOR.darkBlue};
+`;
 
 export default ShadowButton;
