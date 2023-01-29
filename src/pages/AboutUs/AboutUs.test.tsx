7e9592ac590abd9c2fa454c2ee22d66c1c828909
Kate-Chu <96465125+Kate-Chu@users.noreply.github.com>
2023-01-29 07:11:30 +0000

    Deploying to gh-pages from @ Kate-Chu/pawsitive@5c57b0bdda46d6516ec997e2d0fd0f16380e74f5 🚀

--- src/pages/AboutUs/AboutUs.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import AboutUs from '.';
-
-test('render AboutUs page', () => {
-  render(<AboutUs />);
-  const subTitle = screen.getByText('我們的使命');
-  expect(subTitle).toBeInTheDocument();
-});
